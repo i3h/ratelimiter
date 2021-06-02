c525034315cf298db55c859bb783bbe0c31b2c11
i3h <noobly314@gmail.com>
2021-06-02 16:21:12 +0800

    add benchmark

--- benchmark/go.mod
+++ benchmark/go.mod
@@ -4,4 +4,7 @@ replace github.com/i3h/ratelimiter => ../
 
 go 1.14
 
-require github.com/i3h/ratelimiter v0.0.0-00010101000000-000000000000
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/i3h/ratelimiter v0.0.0-00010101000000-000000000000
+)
--- benchmark/main.go
+++ benchmark/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-	rl "github.com/i3h/ratelimiter"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	rl.Test()
+	r := gin.New()
+	r.GET("/", Test)
+	go r.Run(":5000")
+	time.Sleep(1 * time.Second)
+
+	counter := 0
+	start := time.Now()
+
+	for {
+		resp, _ := http.Get("http://127.0.0.1:5000")
+		resp.Body.Close()
+		counter++
+		if time.Now().After(start.Add(5 * time.Second)) {
+			break
+		}
+	}
+
+	end := time.Now()
+	elapsed := end.Sub(start)
+
+	fmt.Println()
+	fmt.Printf("Elapsed: %.2fs\n", elapsed.Seconds())
+	fmt.Printf("Counter: %d\n", counter)
+	fmt.Printf("QPS:     %.2f\n", float64(counter)/elapsed.Seconds())
+}
+
+func Test(c *gin.Context) {
+	s := time.Now().String() + "\n"
+	c.String(200, s)
 }
